b5f02278fca7cffaa5bdc327dc7756e28cdded7b
Frenox <70093632+Frenox@users.noreply.github.com>
2024-03-14 17:20:26 +0100

    Base interface
    Demande:
    - type de rendu
    - langage
    - fichier code
    - fichier appels
    
    Complete les trous dans le code fourni

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 from os import *
 from generation_variable import *
+from donneesJSON import *
 
 def execution():
     outputType = askOutputType()
-    filePath = askFilePath()
+    codeLanguage = askLanguage()
+    filePath = askFilePath("a executer")
+    filePath = askFilePath("contenant les appels de la fonction")
     codeFile = formatage_fichier(filePath)
 
 
 
 def askOutputType():
-    out = input("Selectionnez votre type de formattage (AMC ou Moodle): ")
-    if out.lower() in ["moodle", "amc"]:
-        return out
+    type = input("Selectionnez votre type de formattage (AMC ou Moodle): ")
+    if type.lower() in ["moodle", "amc"]:
+        return type
     else:
         print("Ce n'est pas une option valide")
         return askOutputType()
     
-def askFilePath():
-    filPath = input("Entrez la localisation du fichier a executer: ")
+def askFilePath(fileType):
+    filPath = input(f"Entrez la localisation du fichier {fileType}: ")
     if path.exists(filPath):
         return filPath
     else:
         print("Le fichier n'existe pas")
-        return askFilePath()
+        return askFilePath(fileType)
     
+def askLanguage():
+    languageData = getKnownLanguages() ##Need to get the list of the JSON keys
+    languageList = languageData.keys()
+    language = input("Selectionnez le langage (et la version eventuelle) de l'algorithme fourni: ")
+    if language in languageList:
+        return language
+    else:
+        print("Ce n'est pas une option valide")
+        return askLanguage()
+
 execution()
