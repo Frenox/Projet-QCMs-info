18a3fcd5b296054bb457270190882a095f646dd9
NaelFo <114439059+NaelFo@users.noreply.github.com>
2024-03-19 13:58:15 +0100

    Update generation_question_mako.py

--- generation_question_mako.py
+++ generation_question_mako.py
@@ -1,6 +1,6 @@
 from mako.template import Template
 from random import * 
-def generate_question(indice,programme,question,solution,faux):
+def generate_question(indice,programme,question,reponses):
   enonce = r'''
   \element{categorie${indice}}{
     \begin{question}{cat${indice}-quest${indice}}\bareme{formula=NBC-0.5*NMC} 
@@ -17,12 +17,15 @@ def generate_question(indice,programme,question,solution,faux):
     \end{question}
   }
   '''
-  reponses = [r"\bonne{" + reponse + "}"] + [r"\mauvaise{" + elt+ "}" for elt in faux[:3]]
+  reponses = [r"\bonne{" + reponses[0] + "}"] + [r"\mauvaise{" + elt+ "}" for elt in reponses[1:4]]
   shuffle(reponses)
   return Template(enonce).render(indice = indice,programme = programme,question = question,reponses = reponses)
 indice = 1
 programme = "test1.py"
 question = "Que renvoie ce programme ?"
+"""
 solution = "3"
 faux = ["1","2","4"]
-print(generate_question(indice,programme,question,solution,faux))
+"""
+reponses = ["3","1","2","4"] #la bonne réponse est la première dans la liste
+print(generate_question(indice,programme,question,reponses))
