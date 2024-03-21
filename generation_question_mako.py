18c5de0e85c565c21d77b12a5e8d827ab38af75f
NaelFo <114439059+NaelFo@users.noreply.github.com>
2024-03-21 15:34:44 +0100

    ajout short answer

--- generation_question_mako.py
+++ generation_question_mako.py
@@ -1,15 +1,12 @@
 from mako.template import Template
 from random import * 
-def generate_question(programme,question,reponses,extension):
+def generate_question(programme,question,reponses,extension,typeQuestion = None):
   if extension == "amc":
     enonce = r'''
       \begin{question}{question}
         ${question} 
-        ##la question
-
-    ${programme}
-
-        \begin{reponseshoriz}
+        ${programme}
+      \begin{reponseshoriz}
         % for elt in reponses:
           ${elt}
           % endfor
@@ -17,26 +14,32 @@ def generate_question(programme,question,reponses,extension):
       \end{question}
     '''
   elif extension == "moodle":
-    enonce = r'''
-    \begin{multi}{${question}}
-    ${programme}
-    % for elt in reponses:
-          ${elt}
-          % endfor
-  \end{multi} 
+    main = r'''
+      {${question}}
+      ${programme}
+      % for elt in reponses:
+            ${elt}
+            % endfor
     '''
+    if typeQuestion == "multi":
+      enonce = r'\begin{multi}' + main+ r'\end{multi}'
+    elif typeQuestion == "short":
+      enonce = r'\begin{shortanswer}[usecase]' + main+ r'\end{shortanswer}'
+
   if extension == "amc":
     reponses_format= [r"\bonne{" + reponses[0] + "}"] + [r"\mauvaise{" + elt+ "}" for elt in reponses[1:4]]
   elif extension == "moodle":
-    reponses_format = [r"\item*"+ reponses[0]] + [r"\item" + elt for elt in reponses[1:4]]
-  shuffle(reponses_format)
+    if typeQuestion == "multi":
+      reponses_format = [r"\item*"+ reponses[0]] + [r"\item" + elt for elt in reponses[1:4]]
+    elif typeQuestion == "short":
+      reponses_format = [r"\item" + elt for elt in reponses]
+  #shuffle(reponses_format)
   return Template(enonce).render(programme = programme,question = question,reponses = reponses_format)
+"""
 indice = 1
 programme = "test1.py"
 question = "Que renvoie ce programme ?"
-"""
-solution = "3"
-faux = ["1","2","4"]
-"""
 reponses = ["3","1","2","4"] #la bonne réponse est la première dans la liste
-##print(generate_question(programme,question,reponses,"AMC"))
+reponse2= ["3"]
+print(generate_question(programme,question,reponse2,"moodle","short"))
+"""
