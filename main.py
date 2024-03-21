22a0b78aafd7f6d1327b37cadb19b360602188dc
Frenox <70093632+Frenox@users.noreply.github.com>
2024-03-21 13:44:59 +0100

    Fix main

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from os import *
+import sys
 from generation_variable import *
 from donneesJSON import *
 from generation_question_mako import generate_question
 from execution_avec_subproces import execution
 from reponse import rep
 
-def main():
-    splashScreen()
-    outputType = askOutputType()
-    codeLanguage = askLanguage()
-    filePath = askFilePath("a executer")
-    answerPath = askFilePath("contenant les appels de la fonction")
+def main(outputType, codeLanguage, filePath, answerPath):
     codeFile = formatage_fichier(filePath)
     fileReturn = execution(codeFile, codeLanguage)
     answerLists = rep(fileReturn, answerPath)
@@ -21,47 +17,12 @@ def main():
     
     return questionsString
 
-def splashScreen():
-    print("----------------------------------------------------------------------")
-    print("|                                                                    |")
-    print("|                   Bienvenue sur QCM Generator                      |")
-    print("|                                                                    |")
-    print("|                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                    |")
-    print("|                      -By Cerrad las sopas-                         |")
-    print("|                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                    |")
-    print("|                                                                    |")
-    print("----------------------------------------------------------------------")
-    print()
-
-def askOutputType():
-    type = input("Selectionnez votre type de formattage (AMC ou Moodle): ")
-    if type.lower() in ["moodle", "amc"]:
-        return type
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        outputType = sys.argv[1] 
+        codeLanguage = sys.argv[2]
+        filePath = sys.argv[3]
+        answerPath = sys.argv[4]
+        print(main(outputType, codeLanguage, filePath, answerPath))
     else:
-        print("Ce n'est pas une option valide")
-        return askOutputType()
-    
-def askFilePath(fileType):
-    filPath = input(f"Entrez la localisation du fichier {fileType}: ")
-    if path.exists(filPath):
-        return filPath
-    else:
-        print("Le fichier n'existe pas")
-        return askFilePath(fileType)
-    
-def askLanguage():
-    languageData = getKnownLanguages()
-    languageList = languageData.keys()
-    language = input("Selectionnez le langage (et la version eventuelle) de l'algorithme fourni (ou entrez 'l' pour avoir la liste des langages disponibles): ")
-    
-    if language == "l":
-        for el in languageList:
-            print(el)
-        return askLanguage()
-    elif language in languageList:
-        return languageData[language]
-    else:
-        print("Ce n'est pas une option valide")
-        return askLanguage()
-
-print(main())
+        print(main("moodle", "pythonA", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\codeFile.txt", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\answersFile.txt"))
