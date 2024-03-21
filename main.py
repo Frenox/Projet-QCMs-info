23ea1865f4ab05ddbd376f2681d33b8775a737a0
RuneTreeStudio <128747281+RuneTreeStudio@users.noreply.github.com>
2024-03-21 21:24:11 +0100

    Added Argpass

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ from Execution_docker import *
 from generation_question_mako import generate_question
 from execution_avec_subproces import execution
 from reponse import rep
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('outputType', type=str)
+parser.add_argument('codeLanguage', type=str)
+parser.add_argument('filePath', type=str)
+parser.add_argument('answerPath', type=str)
+
+args = parser.parse_args()
 
 def main(outputType, codeLanguage, filePath, answerPath, questionType):
     codeFile = formatage_fichier(filePath)
@@ -24,12 +33,4 @@ def main(outputType, codeLanguage, filePath, answerPath, questionType):
 def getLanguageData(language):
     return getKnownLanguages()[language]
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        outputType = sys.argv[1] 
-        codeLanguage = sys.argv[2]
-        filePath = sys.argv[3]
-        answerPath = sys.argv[4]
-        main(outputType, codeLanguage, filePath, answerPath)
-    else:
-        main("moodle", "pythonA", r"C:\Users\walid\OneDrive\Documents\GitHub\Projet-QCMs-info\codeFile.txt", r"C:\Users\walid\OneDrive\Documents\GitHub\Projet-QCMs-info\answersFile.txt", "multi")
+main(args.outputType, args.codeLanguage, args.filePath, args.answerPath, 'multi')
