88490dc7b74bf4479fb84b385f6a82c67fb06cbe
Frenox <70093632+Frenox@users.noreply.github.com>
2024-03-28 15:16:35 +0100

    Ajout type minted

--- ShellProgram/main.py
+++ ShellProgram/main.py
@@ -6,15 +6,14 @@ from modules.generation import *
 import argparse
 
 
-parser = argparse.ArgumentParser()
+"""parser = argparse.ArgumentParser()
 parser.add_argument('outputType', type=str)
 parser.add_argument('codeLanguage', type=str)
 parser.add_argument('filePath', type=str)
 parser.add_argument('executionPath', type=str)
 parser.add_argument('answerPath', type=str)
-parser.add_argument('--GUIMode', type=str, help='argument optionnel pour la version GUI')
 
-args = parser.parse_args()
+args = parser.parse_args()"""
 
 def main(outputType, codeLanguage, filePath, executionPath, answerPath, questionType):
     languageData = getKnownLanguages()[codeLanguage]
@@ -24,21 +23,23 @@ def main(outputType, codeLanguage, filePath, executionPath, answerPath, question
         executionFile = execFile.read()
         globalFile = codeFile + "\n" + executionFile 
 
-        fileReturn = execution_docker(globalFile, languageData)
+        fileReturn = execution_docker(globalFile, languageData[:3])
         answerLists = rep(fileReturn, answerPath)
 
+        mintedDisplayType = languageData[-1] if len(languageData) == 4 else "text"
+
         for i in range(len(answerLists)):
-            question = generate_question("codeFile{languageData[0]}", "Que renvoie ce programme?", answerLists[i], outputType, questionType)
-            if args.GUIMode:
-                return(question)
-            else:
-                with open(f'Outputs/test{i}.txt', 'w') as f:
-                    f.write(question)
-                    f.close()
+            question = generate_question("codeFile{languageData[0]}", "Que renvoie ce programme?", answerLists[i], outputType, mintedDisplayType, questionType)
+            with open(f'Outputs/test{i}.txt', 'w') as f:
+                f.write(question)
+                f.close()
+
         with open(f'Outputs/codeFile{languageData[0]}', 'w') as f:
                 f.write(codeFile)
                 f.close()
 
+    execFile.close()
 
 
-main(args.outputType, args.codeLanguage, args.filePath, args.answerPath, 'multi')
+main("amc", "python3", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\ShellProgram\tests_files\Codefiles\codeFile.txt", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\ShellProgram\tests_files\executionFiles\executionFile.txt", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\ShellProgram\tests_files\answerFile\answersFile.txt", "multi")
+##main(args.outputType, args.codeLanguage, args.filePath, args.answerPath, 'multi')
