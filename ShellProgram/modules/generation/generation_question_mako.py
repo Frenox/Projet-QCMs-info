f441f2671f18c6b077c6a00092b16d7ff245964c
NaelFo <114439059+NaelFo@users.noreply.github.com>
2024-03-28 15:14:02 +0100

    ajout insertion fichier mako

--- ShellProgram/modules/generation/generation_question_mako.py
+++ ShellProgram/modules/generation/generation_question_mako.py
@@ -1,11 +1,12 @@
 from mako.template import Template
 from random import * 
-def generate_question(programme,question,reponses,extension,typeQuestion = None):
-  if extension == "amc":
+def generate_question(programme,question,reponses,typeOutput,typeMinted,typeQuestion = None):
+  if typeOutput == "amc":
     enonce = r'''
       \begin{question}{question}
         ${question} 
-        ${programme}
+        \inputminted[firstline=1, lastline=5]{${langage}}{${programme}}
+        % endif
       \begin{reponseshoriz}
         % for elt in reponses:
           ${elt}
@@ -13,7 +14,7 @@ def generate_question(programme,question,reponses,extension,typeQuestion = None)
         \end{reponseshoriz} 
       \end{question}
     '''
-  elif extension == "moodle":
+  elif typeOutput == "moodle":
     main = r'''
       {${question}}
       ${programme}
@@ -26,15 +27,15 @@ def generate_question(programme,question,reponses,extension,typeQuestion = None)
     elif typeQuestion == "short":
       enonce = r'\begin{shortanswer}[usecase]' + main+ r'\end{shortanswer}'
 
-  if extension == "amc":
+  if typeOutput == "amc":
     reponses_format= [r"\bonne{" + reponses[0] + "}"] + [r"\mauvaise{" + elt+ "}" for elt in reponses[1:4]]
-  elif extension == "moodle":
+  elif typeOutput == "moodle":
     if typeQuestion == "multi":
       reponses_format = [r"\item*"+ reponses[0]] + [r"\item" + elt for elt in reponses[1:4]]
     elif typeQuestion == "short":
       reponses_format = [r"\item" + elt for elt in reponses]
   #shuffle(reponses_format)
-  return Template(enonce).render(programme = programme,question = question,reponses = reponses_format)
+  return Template(enonce).render(programme = programme,question = question,reponses = reponses_format,langage = typeMinted )
 """
 indice = 1
 programme = "test1.py"
