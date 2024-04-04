c782036ed433daee723fd8e771e8593822c28c13
Frenox <70093632+Frenox@users.noreply.github.com>
2024-04-04 16:06:08 +0200

    Update main
    Ajout arguments (nom, type question)

--- ShellProgram/main.py
+++ ShellProgram/main.py
@@ -6,22 +6,24 @@ from modules.generation import *
 import argparse
 
 
-"""parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
+parser.add_argument('questionName', type=str)
 parser.add_argument('outputType', type=str)
 parser.add_argument('codeLanguage', type=str)
 parser.add_argument('filePath', type=str)
 parser.add_argument('executionPath', type=str)
 parser.add_argument('answerPath', type=str)
+parser.add_argument("questionType", nargs='?', default="multi")
 
-args = parser.parse_args()"""
+args = parser.parse_args()
 
-def main(outputType, codeLanguage, filePath, executionPath, answerPath, questionType):
+def main(questionName, outputType, codeLanguage, filePath, executionPath, answerPath, questionType):
     languageData = getKnownLanguages()[codeLanguage] ## Recupere les donnees associees au langage demande
     codeFile = formatage_fichier(filePath) ## Remplace les balises par du code dans le fichier donne
 
     with open(executionPath, "r") as execFile:
         executionFile = execFile.read()
-        categoryList, cleanExecFile = handleCategories(executionFile)
+        categoryList, cleanExecFile = handleCategories(questionName, executionFile)
         globalFile = codeFile + "\n" + cleanExecFile ## Cree le fichier global contenant le code et les appels a executer
 
         fileReturn = execution_docker(globalFile, languageData[:3]) ## Execute le fichier sur docker (supprime le 4eme element de la liste s'il existe (non necessaire pour cette partie))
@@ -34,11 +36,12 @@ def main(outputType, codeLanguage, filePath, executionPath, answerPath, question
             formatedQuestionsList.append(generate_question(f"codeFile{languageData[0]}", "Que renvoie ce programme?", answerLists[i], outputType, mintedDisplayType, questionType)) ## Genere le formattage de la question
             
         questionsDict = handleQuestionGroups(formatedQuestionsList,categoryList)
-        for category, questions in questionsDict.items():
-            categoryString = generate_categorie(category,questions)
-            with open(f'Outputs/{category}.txt', 'w') as f:
+
+        with open(f"Outputs/{questionName}.txt", "w") as f:
+            for category, questions in questionsDict.items():
+                categoryString = generate_categorie(category,questions)
                 f.write(categoryString) ## Cree le fichier contenant la question
-                f.close()
+        f.close()
 
         with open(f'Outputs/codeFile{languageData[0]}', 'w') as f:
                 f.write(codeFile) ## Cree le fichier contenant le code (pour affichage en latex)
@@ -46,7 +49,7 @@ def main(outputType, codeLanguage, filePath, executionPath, answerPath, question
 
     execFile.close()
 
-def handleCategories(executionFile):
+def handleCategories(questionName, executionFile):
     lines = executionFile.split("\n")
 
     categoryList = []
@@ -57,7 +60,7 @@ def handleCategories(executionFile):
     for line in lines:
         if line.replace(" ","") != "\n" and len(line) > 0:
             if line[0] == "#":
-                currentCategory = line[1:]
+                currentCategory = questionName + "_" + line[1:]
             else:
                 callsString += line + "\n"
                 categoryList.append(currentCategory)
@@ -72,5 +75,4 @@ def handleQuestionGroups(questionsList, categoryList):
 
 
 
-main("amc", "python3", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\ShellProgram\tests_files\Codefiles\codeFile.txt", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\ShellProgram\tests_files\executionFiles\executionFile.txt", r"C:\Users\maxim\Desktop\INP\2A\Projet\Projet-QCMs-info\ShellProgram\tests_files\answerFile\answersFile.txt", "multi")
-##main(args.outputType, args.codeLanguage, args.filePath, args.answerPath, 'multi')
+main(args.questionName, args.outputType, args.codeLanguage, args.filePath, args.executionPath, args.answerPath, args.questionType)
