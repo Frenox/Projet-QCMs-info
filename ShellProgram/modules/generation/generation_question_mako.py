f731e90a819aa7bf8cb017a3e663bf78adb811cb
NaelFo <114439059+NaelFo@users.noreply.github.com>
2024-04-11 14:48:37 +0200

    changement génération questions

--- ShellProgram/modules/generation/generation_question_mako.py
+++ ShellProgram/modules/generation/generation_question_mako.py
@@ -1,8 +1,9 @@
 from mako.template import Template
 from random import * 
-def generate_question(programme,question,reponses,typeOutput,typeMinted,typeQuestion = "None"):
+def generate_question(programme,question,reponses,typeOutput,typeMinted,category = None,typeQuestion = None):
   if typeOutput == "amc":
     enonce = r'''
+    \element{${category}}{
       \begin{question}{question}
         ${question} 
         \inputminted[firstline=1, lastline=5]{${langage}}{${programme}}
@@ -12,9 +13,14 @@ def generate_question(programme,question,reponses,typeOutput,typeMinted,typeQues
           % endfor
         \end{reponseshoriz} 
       \end{question}
+      }
     '''
     reponses_format= [r"\bonne{" + reponses[0] + "}"] + [r"\mauvaise{" + elt+ "}" for elt in reponses[1:4]]
   elif typeOutput == "moodle":
+    if category is not None:
+      enonce = r'''\setsubcategory{''' + category + '}'
+    else:
+      enonce =''
     main = r'''
       {${question}}
       \inputminted[firstline=1, lastline=5]{${langage}}{${programme}}
@@ -23,32 +29,31 @@ def generate_question(programme,question,reponses,typeOutput,typeMinted,typeQues
             % endfor
     '''
     if typeQuestion == "multi":
-      enonce = r'\begin{multi}' + main+ r'\end{multi}'
+      enonce += r'\begin{multi}' + main+ r'\end{multi}'
       reponses_format = [r"\item*"+ reponses[0]] + [r"\item" + elt for elt in reponses[1:4]]
     elif typeQuestion == "short":
-      enonce = r'\begin{shortanswer}[usecase]' + main+ r'\end{shortanswer}'
+      enonce += r'\begin{shortanswer}[usecase]' + main+ r'\end{shortanswer}'
       reponses_format = [r"\item" + elt for elt in reponses]
-  #shuffle(reponses_format)
-  return Template(enonce).render(programme = programme,question = question,reponses = reponses_format,langage = typeMinted )
+  return Template(enonce).render(programme = programme,question = question,reponses = reponses_format,langage = typeMinted,category = category)
 
-def generate_categorie(nom,liste_questions):
-  code = r'''\element{${nom}}{
-  % for elt in questions:
-  ${elt}
-  % endfor
+
+def generate_category(nom,categories):
+  code = r'''
+  % for elt in categories:
+  \setgroupmode{${elt[0]}}{cyclic}
+  \shufflegroup{${elt[0]}}
+  % for _ in range(elt[1]):
+  \element{${nom}}{
+  \restituegroupe[1]{${elt[0]}}
   }
+  % endfor
+  % endfor
     '''
-  return Template(code).render(nom = nom,questions = liste_questions )
+  return Template(code).render(nom = nom,categories = categories )
+
 
 
 """
-indice = 1
-programme = "test1.py"
-question = "Que renvoie ce programme ?"
-reponses = ["3","1","2","4"] #la bonne réponse est la première dans la liste
-reponse2= ["3"]
-question1 = generate_question(programme,question,reponses,"amc","python")
-question2 = generate_question(programme,question,reponses,"amc","python")
-#print(generate_question(programme,question,reponses,"moodle","python","multi"))
-print(generate_categorie("cat",[question1,question2]))
+print(generate_question("test1.py","Que ?","47","moodle","python","newCat","short"))
+print(generate_category("Category3",[("Category2",2),("Category1",1)]))
 """
