7b77c6598f7b0d6046544be4f874c512944a7009
NaelFo <114439059+NaelFo@users.noreply.github.com>
2024-04-20 11:10:50 +0200

    documentation generation_question_mako

--- common_modules/generation/generation_question_mako.py
+++ common_modules/generation/generation_question_mako.py
@@ -1,12 +1,27 @@
 from mako.template import Template
 from random import * 
 def generate_question(program,question,answers,typeOutput,typeMinted,category,typeQuestion = None):
+  """
+    Genere une question d'un QCM en latex au format moodle.sty ou AMC
+    Inputs:
+       *program(str): Nom du fichier contenant le programme utilise pour la question (ex: "program.py")
+       *question (str) : Enonce de la question
+       *answers(list): Liste des reponses, le premier element est la bonne reponse et les autres les mauvaises reponses(
+       dans le cas d'une question à choix multiples)
+       *typeOutput(str): Specifie le format qu'on souhaite (pour l'instant AMC ou moodle)
+       *typeMinted(str): Precise le langage a rentre pour que minted mette en forme le code (ex: cpp pour un fichier en c++)
+       *category (str) : Nom du groupe de la question, permet de regrouper des questions en AMC et moodle
+       *typeQuestion(str): Precise le type de question pour moodle, pour l'instant seul les categories shortanswer(argument: short)
+       et multi sont implementes
+    Outputs:
+        Template(enonce).render(...): str contenant le code latex correspondant a la question
+    """
   if typeOutput == "amc":
     enonce = r'''
     \element{${category}}{
       \begin{question}{question}
         ${question} 
-        \inputminted[firstline=1, lastline=5]{${langage}}{${programme}}
+        \inputminted{${langage}}{${programme}}
       \begin{reponseshoriz}
         % for elt in answers:
           ${elt}
@@ -20,7 +35,7 @@ def generate_question(program,question,answers,typeOutput,typeMinted,category,ty
     enonce = r'''\setsubcategory{''' + category + '}'
     main = r'''
       {${question}}
-      \inputminted[firstline=1, lastline=5]{${langage}}{${programme}}
+      \inputminted{${langage}}{${programme}}
       % for elt in answers:
             ${elt}
             % endfor
@@ -31,10 +46,23 @@ def generate_question(program,question,answers,typeOutput,typeMinted,category,ty
     elif typeQuestion == "short":
       enonce += r'\begin{shortanswer}[usecase]' + main+ r'\end{shortanswer}'
       answers_format = [r"\item" + elt for elt in answers]
+    else:
+      return "format non pris en compte"
+  else:
+    return "format non pris en compte"
   return Template(enonce).render(programme = program,question = question,answers = answers_format,langage = typeMinted,category = category)
 
 
 def generate_category(name,categories):
+  """
+    Pour AMC, genere une categorie regroupant un nombre choisi de question d'autres categories 
+    Inputs:
+       *name(str): Nom de la categorie cree
+       *categories (str) : dictionnaire de cette forme: {nameCategory: numberQuestions} ab=vec nameCategory(str) le nom
+       d'une categorie et numberQuestions(int) le nmbre de question de cette categorie a inclure
+    Outputs:
+        Template(code).render(...): str contenant le code latex correspondant a la categorie cree en AMC
+    """
   code = r'''
   % for elt in categories.keys():
   \setgroupmode{${elt}}{cyclic}
@@ -47,10 +75,5 @@ def generate_category(name,categories):
   % endfor
     '''
   return Template(code).render(name = name,categories = categories )
-
-
-
-print(generate_question("test1.py","Que ?",["47"],"moodle","python","newCat","short"))
-print(generate_category("Category3",{"Category2":2,"Category1":1}))
 
 
